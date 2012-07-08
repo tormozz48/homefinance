1db95f144d58953b5403d58fb96714af1b312201
Andrey Kuznetsov <tormozz48@gmail.com>
2012-07-08 09:39:40 +0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,8 @@ module ApplicationHelper
   def user_full_name
     if !current_user.nil?
         if (!current_user.first_name.nil? &&
-           !current_user.last_name.nil? &&
-           !current_user.first_name.empty? &&
-           !current_user.last_name.empty?)
+           !current_user.last_name.nil? )
           return current_user.first_name + " " + current_user.last_name
-        elsif (!current_user.first_name.nil?
-              !current_user.first_name.empty?)
-          return current_user.first_name
-        elsif (!current_user.last_name.nil? &&
-              !current_user.last_name.empty?)
-          return current_user.last_name
         else
           return current_user.email
         end

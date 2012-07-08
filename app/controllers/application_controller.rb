1a88fb99a4e70fda081ac3aceaf6c6f626d910e9
Andrey Kuznetsov <tormozz48@gmail.com>
2012-07-08 14:51:23 +0300

    social authentification has been implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,6 +64,32 @@ class ApplicationController < ActionController::Base
   end
 
   def sign_with_social
-
+    token = params[:token]
+    if !token.nil?
+      require 'net/http'
+      url = URI.parse('http://ulogin.ru/token.php?token='+token)
+      social_data = ActiveSupport::JSON.decode(Net::HTTP.get(url))
+      uid = social_data['identity']
+      email = social_data['email']
+      first_name = social_data['first_name']
+      last_name = social_data['last_name']
+      @user = User.find_by_email(email)
+      if @user.nil?
+        @user = User.find_by_authentication_token(uid)
+        my_time = Time.now
+        if @user.nil?
+          @user = User.new
+          @user.email=email
+          @user.password="password"
+          @user.authentication_token=uid
+          @user.first_name=first_name
+          @user.last_name=last_name
+          @user.skip_confirmation!
+          @user.save
+        end
+      end
+      sign_in @user, :bypass => true
+      redirect_to root_path
+    end
   end
 end

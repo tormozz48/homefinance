ac41096295e68b59ac3db087cfd5f549ec3521ea
Andrey Kuznetsov <tormozz48@gmail.com>
2012-07-13 10:58:28 +0300

    fix locale missing error

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,21 +1,21 @@
 module StatisticsHelper
   def getTransactionTypesOptionList
     str = ""
-    str += "<option value="+7.to_s(10)+">"+I18n.t(:menu_transaction_from_cash)+"</option>"
-    str += "<option value="+2.to_s(10)+">"+I18n.t(:menu_transaction_from_account_to_cash)+"</option>"
-    str += "<option value="+0.to_s(10)+">"+I18n.t(:menu_transaction_to_account)+"</option>"
-    str += "<option value="+4.to_s(10)+">"+I18n.t(:menu_transaction_to_cash)+"</option>"
-    str += "<option value="+6.to_s(10)+">"+I18n.t(:menu_transaction_from_cash_to_cash)+"</option>"
-    str += "<option value="+3.to_s(10)+">"+I18n.t(:menu_transaction_from_account_to_category)+"</option>"
-    str += "<option value="+1.to_s(10)+">"+I18n.t(:menu_transaction_from_account_to_account)+"</option>"
-    str += "<option value="+5.to_s(10)+">"+I18n.t(:menu_transaction_from_cash_to_account)+"</option>"
+    str += "<option value="+7.to_s(10)+">"+I18n.t('menu.transaction.from_cash.to_category')+"</option>"
+    str += "<option value="+2.to_s(10)+">"+I18n.t('menu.transaction.from_account.to_cash')+"</option>"
+    str += "<option value="+0.to_s(10)+">"+I18n.t('menu.transaction.to_account')+"</option>"
+    str += "<option value="+4.to_s(10)+">"+I18n.t('menu.transaction.to_cash')+"</option>"
+    str += "<option value="+6.to_s(10)+">"+I18n.t('menu.transaction.from_cash.to_cash')+"</option>"
+    str += "<option value="+3.to_s(10)+">"+I18n.t('menu.transaction.from_account.to_category')+"</option>"
+    str += "<option value="+1.to_s(10)+">"+I18n.t('menu.transaction.from_account.to_account')+"</option>"
+    str += "<option value="+5.to_s(10)+">"+I18n.t('menu.transaction.from_cash.to_account')+"</option>"
     return str
   end
 
   def getTransactionTypesWithCategoriesOptionList
     str = ""
-    str += "<option value="+7.to_s(10)+">"+I18n.t(:menu_transaction_from_cash)+"</option>"
-    str += "<option value="+3.to_s(10)+">"+I18n.t(:menu_transaction_from_account_to_category)+"</option>"
+    str += "<option value="+7.to_s(10)+">"+I18n.t('menu.transaction.from_cash.to_category')+"</option>"
+    str += "<option value="+3.to_s(10)+">"+I18n.t('menu.transaction.from_account.to_category')+"</option>"
     return str
   end
 

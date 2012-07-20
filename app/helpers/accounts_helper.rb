88cd38f34870c70817b55a9752624f60e48c6796
Andrey Kuznetsov <tormozz48@gmail.com>
2012-07-20 18:16:14 +0300

    third commit for adaptive design

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -19,9 +19,9 @@ module AccountsHelper
   def getAddButtonLink(type)
     title = ""
     if @account_type == getAccountCardType.to_s(10)
-      title = I18n.t('add.account.card')
+      title = I18n.t('add.item')
     elsif @account_type == getAccountCashType.to_s(10)
-      title = I18n.t('add.account.cash')
+      title = I18n.t('add.item')
     end
     return link_to title , new_account_path(:type => @account_type), :class => "link"
   end

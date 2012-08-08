12395d743928293ebbbbb6840edc06b237953257
Andrey Kuznetsov <tormozz48@gmail.com>
2012-08-08 23:55:11 +0300

    fix errors after code refactoring

--- config/application.rb
+++ config/application.rb
@@ -56,15 +56,5 @@ module Homefinance2
     config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    #transaction types
-    TRANSACTION_TO_ACCOUNT = 0
-    TRANSACTION_FROM_ACCOUNT_TO_ACCOUNT = 1
-    TRANSACTION_FROM_ACCOUNT_TO_CASH = 2
-    TRANSACTION_FROM_ACCOUNT_TO_CATEGORY = 3
-    TRANSACTION_TO_CASH = 4
-    TRANSACTION_FROM_CASH_TO_ACCOUNT = 5
-    TRANSACTION_FROM_CASH_TO_CASH = 6
-    TRANSACTION_FROM_CASH_TO_CATEGORY = 7
   end
 end

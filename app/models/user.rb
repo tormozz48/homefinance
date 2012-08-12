7be9e61c01a6c004892e7b3314afc967041eae92
Andrey Kuznetsov <tormozz48@gmail.com>
2012-08-12 16:13:31 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,12 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth access_token
     logger.info "find for facebook start"
     if user = User.where(:url => access_token.info.urls.Facebook).first
-      logger.info "user found"
-      user
+      logger.info "user found by facebook token id"
+      return user
+    elsif
+      user = User.where(:email => access_token.extra.raw_info.email)
+      logger.info "user found by email"
+      return user
     else
       logger.info "start create user"
       user = User.new(:provider => access_token.provider,

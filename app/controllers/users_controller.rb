535a9a8cb0d788cc64b26e732108720e64dedc0d
Andrey Kuznetsov <tormozz48@gmail.com>
2012-08-13 16:10:21 +0300

    auth fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,18 +21,13 @@ class UsersController < Devise::OmniauthCallbacksController
   end
 
   def facebook
-    fbUser = User.find_for_facebook_oauth request.env["omniauth.auth"]
-    if fbUser.nil? == false
+    @user = User.find_for_facebook_oauth request.env["omniauth.auth"]
+    if @user.nil? == false
       logger.info "user found in database"
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       logger.info "user sign in start"
-      #sign_in fbUser, :bypass => true
-      #logger.info "user sign in end"
-      #redirect_to root_path
-      sign_in_and_redirect fbUser, :event => :authentication
-    else
-      logger.info "auth error"
-      flash[:notice] = "authentication error"
+      sign_in @user, :bypass => true
+      logger.info "user sign in end"
       redirect_to root_path
     end
   end

f528bb5248f79048c0b1723ba43f9b4ffec31a66
andrey <tormozz48@gmail.com>
2013-03-01 10:51:43 +0200

    remove food diary functionality from application

--- app/models/eating_type.rb
+++
@@ -1,6 +0,0 @@
-class EatingType < ActiveRecord::Base
-  validates :enabled, :name, :eating_order, :presence => true
-  validates :name, :length => {:minimum => 3, :maximum => 30}
-  validates :eating_order, :numericality => {:greater_than_or_equal_to => 0 }
-  validate :enabled, :inclusion => { :in => [true, false] }
-end

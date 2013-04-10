f8e95cc494735c8afefab4de09b6981ec647226e
andrey <tormozz48@gmail.com>
2013-04-10 17:30:02 +0300

    add sorting features to column headers for categories and accounts

--- app/assets/javascripts/accounts_index.js
+++ app/assets/javascripts/accounts_index.js
@@ -5,19 +5,18 @@ AccountsIndex = function(config){
 AccountsIndex.prototype = {
     config: null,
 
+    SORT_ASC: 'asc',
+    SORT_DESC: 'desc',
+
     $data_wrapper: null,
-    $sort_field: null,
-    $sort_direction: null,
+    sort_field: 'name',
+    sort_direction: this.SORT_ASC,
 
     init: function(config){
         this.config = config;
         //var self = this;
 
         this.$data_wrapper = jQuery('#data_wrapper');
-        this.$sort_field = jQuery('#field');
-        this.$sort_direction = jQuery('#direction');
-
-//        this.init_tooltip();
 
         if(this.config.account_type == this.config.ACCOUNT_TYPE_CARD){
             mark_menu("menuAccountsID");
@@ -25,27 +24,40 @@ AccountsIndex.prototype = {
             mark_menu("menuCashesID");
         }
 
-//        this.$sort_field.chosen();
-//        this.$sort_direction.chosen();
-//
-//        this.$sort_field.on('change', function(){
-//            self.load_data();
-//        });
-//
-//        this.$sort_direction.on('change', function(){
-//            self.load_data();
-//        });
+        var self = this;
+        jQuery('.sorting-header').click(function(){
+            var sort_field = jQuery(this).attr('data_field');
+
+            if(self.sort_field == sort_field){
+                self.sort_direction = self.sort_direction == self.SORT_ASC ? self.SORT_DESC : self.SORT_ASC;
+            }else{
+                self.sort_direction = self.SORT_ASC;
+            }
+
+            self.sort_field = sort_field;
+            self.toggle_arrows();
+            self.load_data();
+        });
 
         this.load_data();
     },
 
+    toggle_arrows: function(){
+        jQuery('th > i').addClass('no-disp');
+
+        var $sorted_header = jQuery('[data_field = "' + this.sort_field + '"]');
+        if(this.sort_direction == this.SORT_ASC){
+            $sorted_header.parent().children('.icon-arrow-up').removeClass('no-disp');
+        }else{
+            $sorted_header.parent().children('.icon-arrow-down').removeClass('no-disp');
+        }
+    },
+
     load_data: function(){
-        var field = this.$sort_field.val();
-        var direction = this.$sort_direction.val();
         var self = this;
         jQuery.ajax({
             url: "/accounts/load",
-            data: {field: field, direction: direction, type: this.config.account_type},
+            data: {field: this.sort_field, direction: this.sort_direction, type: this.config.account_type},
             success: function(data){
                 self.handleResponse(data);
             }
--- app/assets/javascripts/categories_index.js
+++ app/assets/javascripts/categories_index.js
@@ -5,43 +5,56 @@ CategoriesIndex = function(config){
 CategoriesIndex.prototype = {
 	config: null,
 
+    SORT_ASC: 'asc',
+    SORT_DESC: 'desc',
+
     $data_wrapper: null,
-    $sort_field: null,
-    $sort_direction: null,
+    sort_field: 'name',
+    sort_direction: this.SORT_ASC,
 
 	init: function(config){
 		this.config = config;
-        //var self = this;
 
         this.$data_wrapper = jQuery('#data_wrapper');
-        this.$sort_field = jQuery('#field');
-        this.$sort_direction = jQuery('#direction');
 
         this.init_tooltip();
 
         mark_menu("menuCategoriesID");
 
-//        this.$sort_field.chosen();
-//        this.$sort_direction.chosen();
-//
-//        this.$sort_field.on('change', function(){
-//           self.load_data();
-//        });
-//
-//        this.$sort_direction.on('change', function(){
-//            self.load_data();
-//        });
+        var self = this;
+        jQuery('.sorting-header').click(function(){
+            var sort_field = jQuery(this).attr('data_field');
+
+            if(self.sort_field == sort_field){
+               self.sort_direction = self.sort_direction == self.SORT_ASC ? self.SORT_DESC : self.SORT_ASC;
+            }else{
+               self.sort_direction = self.SORT_ASC;
+            }
+
+            self.sort_field = sort_field;
+            self.toggle_arrows();
+            self.load_data();
+        });
 
         this.load_data();
 	},
 
+    toggle_arrows: function(){
+        jQuery('th > i').addClass('no-disp');
+
+        var $sorted_header = jQuery('[data_field = "' + this.sort_field + '"]');
+        if(this.sort_direction == this.SORT_ASC){
+            $sorted_header.parent().children('.icon-arrow-up').removeClass('no-disp');
+        }else{
+            $sorted_header.parent().children('.icon-arrow-down').removeClass('no-disp');
+        }
+    },
+
     load_data: function(){
-        var field = this.$sort_field.val();
-        var direction = this.$sort_direction.val();
         var self = this;
         jQuery.ajax({
             url: "/categories/load",
-            data: {field: field, direction: direction},
+            data: {field: this.sort_field, direction: this.sort_direction},
             success: function(data){
                 self.handleResponse(data);
             }
